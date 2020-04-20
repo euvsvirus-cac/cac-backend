b22e604917c1bdf0a9008f4268eed3c8fa7dc8a2
Nils Knudsen <n.knudsen@monday-consulting.com>
2020-04-20 22:38:34 +0200

    fix error

--- src/main/resources/db/migration/V1.0.1__create_default_user.sql
+++ src/main/resources/db/migration/V1.0.1__create_default_user.sql
@@ -1,1 +1,1 @@
-INSERT INTO "user" VALUES (1, 'Ramesh', 'Ahmedabad', 'nk@abc.de', '$2y$12$QSsSSMRDGtBIJOZL0f7ZHuSCZXP./giQu4lYJv1gTkckiUiAHWQl.');
+INSERT INTO user VALUES (1, 'Ramesh', 'Ahmedabad', 'nk@abc.de', '$2y$12$QSsSSMRDGtBIJOZL0f7ZHuSCZXP./giQu4lYJv1gTkckiUiAHWQl.');
