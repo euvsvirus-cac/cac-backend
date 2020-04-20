34c88f2db1ea8a35940bb65fcb7f04368c1c375a
Oliver Degener <hg.otothel@gmail.com>
2020-04-20 16:34:42 +0200

    Project setup with Spring Boot

---
+++ src/main/java/org/euvsvirus/cac/CacBackendApplication.java
@@ -0,0 +1,13 @@
+package org.euvsvirus.cac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CacBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/euvsvirus/cac/CacBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.euvsvirus.cac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
