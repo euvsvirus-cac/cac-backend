32706bb175454cab1ae2d05d6f78de145b47ceae
Nils Knudsen <n.knudsen@monday-consulting.com>
2020-04-21 17:27:05 +0200

    user creation and login / check login with bearer

--- src/main/java/org/euvsvirus/cac/controller/UserController.java
+++ src/main/java/org/euvsvirus/cac/controller/UserController.java
@@ -1,19 +1,21 @@
 package org.euvsvirus.cac.controller;
 
-import org.euvsvirus.cac.model.request.LoginRequest;
+import org.euvsvirus.cac.model.JwtToken;
 import org.euvsvirus.cac.model.User;
+import org.euvsvirus.cac.model.request.LoginRequest;
 import org.euvsvirus.cac.model.response.JWTTokenResponse;
+import org.euvsvirus.cac.service.CacUserDetailsService;
 import org.euvsvirus.cac.service.CacUserLoginService;
-import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
@@ -21,6 +23,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final CacUserLoginService cacUserLoginService;
+
+    @Autowired
+    private CacUserDetailsService cacUserDetailsService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtToken jwtToken;
+
 
     public UserController(CacUserLoginService cacUserLoginService) {
         this.cacUserLoginService = cacUserLoginService;
@@ -35,9 +47,36 @@ public class UserController {
         return null;
     }
 
-    @PostMapping(value = "/login", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<JWTTokenResponse> login(@RequestBody LoginRequest login) {
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody LoginRequest loginRequest) throws Exception {
 
-        return new ResponseEntity<>(cacUserLoginService.login(login.getEmail(), login.getPassword()), HttpStatus.OK);
+
+        authenticate(loginRequest.getEmail(), loginRequest.getPassword());
+
+        final UserDetails userDetails = cacUserDetailsService.loadUserByUsername(loginRequest.getEmail());
+
+        final String token = jwtToken.generateToken(userDetails);
+
+        return ResponseEntity.ok(new JWTTokenResponse(token));
+
     }
+
+    private void authenticate(String username, String password) throws Exception {
+
+        try {
+
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+
+        } catch (DisabledException e) {
+
+            throw new Exception("USER_DISABLED", e);
+
+        } catch (BadCredentialsException e) {
+
+            throw new Exception("INVALID_CREDENTIALS", e);
+
+        }
+
+    }
+
 }
