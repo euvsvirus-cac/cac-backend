63fa4eaa8348e658d13e0e8865e6a532ff9ceb1c
Oliver Degener <hg.otothel@gmail.com>
2020-04-27 01:18:32 +0200

    Add demo data 'demo-euvsvirus'

---
+++ src/main/resources/db/migration/V1.0.2__create_euvsvirus_demo_data.sql.sql
@@ -0,0 +1,39 @@
+INSERT INTO team VALUES ('ba45dcdb-f4b9-4239-9e9b-e38e5d63f0d9', 'EUvsVirus', 'demo-euvsvirus');
+
+INSERT INTO user VALUES ('2f3d24d7-3c3e-42d7-b92b-19034dba1b65', 'Oliver Degener', null, 'Software Engineer / Student', 1,  '7degener@informatik.uni-hamburg.de', '$2y$12$.v3T7udIYzFrV5V/3dfznuzHxZ95tmcBnKXUCQXOOaSEkaEKe5yhu', 'ba45dcdb-f4b9-4239-9e9b-e38e5d63f0d9', 'U012WCGQAGG');
+INSERT INTO user VALUES ('9d451193-83b7-4e0a-9f3d-c26d0db484e2', 'Nils Knudsen', null, null, 0,  'nils@localhost', '$2y$12$QSsSSMRDGtBIJOZL0f7ZHuSCZXP./giQu4lYJv1gTkckiUiAHWQl.', 'ba45dcdb-f4b9-4239-9e9b-e38e5d63f0d9', 'U012CN3LVQU');
+INSERT INTO user VALUES ('75a8ba57-686a-4251-8696-3ca8745781fd', 'Christopher Soh', null, 'Creatives / Marketing', 0,  'chris@localhost', '$2y$12$HNqUf6WndoViqO7akf0HSuJ.4rUEI5Uaxuq0UwIn2LCcXql4bWTgG', 'ba45dcdb-f4b9-4239-9e9b-e38e5d63f0d9', 'U0135RQRU48');
+
+INSERT INTO user VALUES ('bf6b255d-697d-4b32-bd82-cad6b03beac0', 'Kadeem Slater', null, null, 1,  'kadeem@localhost', '$2y$12$.v3T7udIYzFrV5V/3dfznuzHxZ95tmcBnKXUCQXOOaSEkaEKe5yhu', 'ba45dcdb-f4b9-4239-9e9b-e38e5d63f0d9', null);
+INSERT INTO user VALUES ('1f4a386c-c934-4911-b129-971ddc03c71a', 'Aamir Gonzales', null, null, 1,  'aamir@localhost', '$2y$12$.v3T7udIYzFrV5V/3dfznuzHxZ95tmcBnKXUCQXOOaSEkaEKe5yhu', 'ba45dcdb-f4b9-4239-9e9b-e38e5d63f0d9', null);
+INSERT INTO user VALUES ('f6b16f15-3e95-4bea-a7d9-44d6a7a9f6ce', 'Carys Mendez', null, null, 1,  'carys@localhost', '$2y$12$.v3T7udIYzFrV5V/3dfznuzHxZ95tmcBnKXUCQXOOaSEkaEKe5yhu', 'ba45dcdb-f4b9-4239-9e9b-e38e5d63f0d9', null);
+INSERT INTO user VALUES ('f0894a63-5e24-46a4-a9b6-21fb742f6473', 'Julia Stokes', null, null, 0,  'julia@localhost', '$2y$12$.v3T7udIYzFrV5V/3dfznuzHxZ95tmcBnKXUCQXOOaSEkaEKe5yhu', 'ba45dcdb-f4b9-4239-9e9b-e38e5d63f0d9', null);
+INSERT INTO user VALUES ('e13e5f9e-6787-4452-896b-78565017b6b7', 'Yasemin Mohammed', null, null, 0,  'yasemin@localhost', '$2y$12$.v3T7udIYzFrV5V/3dfznuzHxZ95tmcBnKXUCQXOOaSEkaEKe5yhu', 'ba45dcdb-f4b9-4239-9e9b-e38e5d63f0d9', null);
+INSERT INTO user VALUES ('a0a86195-aa87-413f-9067-ee07b9cdb821', 'Velma Ortiz', null, null, 0,  'velma@localhost', '$2y$12$.v3T7udIYzFrV5V/3dfznuzHxZ95tmcBnKXUCQXOOaSEkaEKe5yhu', 'ba45dcdb-f4b9-4239-9e9b-e38e5d63f0d9', null);
+
+INSERT INTO skill VALUES ('dbbf1d00-07bd-4281-b2bd-74f86160a90b', 'java', 'ba45dcdb-f4b9-4239-9e9b-e38e5d63f0d9');
+INSERT INTO skill VALUES ('3763deac-3a99-463d-9e1a-e7ea46116309', 'react', 'ba45dcdb-f4b9-4239-9e9b-e38e5d63f0d9');
+INSERT INTO skill VALUES ('74e3c897-ab44-45b5-8646-620446c28778', 'sql', 'ba45dcdb-f4b9-4239-9e9b-e38e5d63f0d9');
+INSERT INTO skill VALUES ('31746e62-e300-443b-9dea-ea94d6924fd2', 'video-editing', 'ba45dcdb-f4b9-4239-9e9b-e38e5d63f0d9');
+INSERT INTO skill VALUES ('5bbefeb2-4534-42be-ac0c-ff2b4f9f407c', 'marketing', 'ba45dcdb-f4b9-4239-9e9b-e38e5d63f0d9');
+INSERT INTO skill VALUES ('9a5c83a9-155b-4660-8634-70f854a9f1f3', 'jenkins', 'ba45dcdb-f4b9-4239-9e9b-e38e5d63f0d9');
+INSERT INTO skill VALUES ('81357686-0d05-4e6a-947b-f09c9fc14ad2', 'docker', 'ba45dcdb-f4b9-4239-9e9b-e38e5d63f0d9');
+INSERT INTO skill VALUES ('1f8b5218-c45f-42b9-9995-f7f71c0c0645', 'spring-framework', 'ba45dcdb-f4b9-4239-9e9b-e38e5d63f0d9');
+INSERT INTO skill VALUES ('97a3a207-828e-43be-8865-1903434a0a26', 'excel', 'ba45dcdb-f4b9-4239-9e9b-e38e5d63f0d9');
+INSERT INTO skill VALUES ('431e353d-682d-4f09-af85-9dfe72da0866', 'arduino', 'ba45dcdb-f4b9-4239-9e9b-e38e5d63f0d9');
+INSERT INTO skill VALUES ('e72610b2-3c72-4036-b111-ac6c9a21ad3e', 'accounting', 'ba45dcdb-f4b9-4239-9e9b-e38e5d63f0d9');
+INSERT INTO skill VALUES ('18edc2c2-e6b6-4f04-9181-1640c2813a7a', 'photoshop', 'ba45dcdb-f4b9-4239-9e9b-e38e5d63f0d9');
+
+INSERT INTO user_skill VALUES ('2f3d24d7-3c3e-42d7-b92b-19034dba1b65', 'dbbf1d00-07bd-4281-b2bd-74f86160a90b', 'GOLD');
+INSERT INTO user_skill VALUES ('2f3d24d7-3c3e-42d7-b92b-19034dba1b65', '3763deac-3a99-463d-9e1a-e7ea46116309', 'BRONZE');
+INSERT INTO user_skill VALUES ('2f3d24d7-3c3e-42d7-b92b-19034dba1b65', '74e3c897-ab44-45b5-8646-620446c28778', 'SILVER');
+INSERT INTO user_skill VALUES ('2f3d24d7-3c3e-42d7-b92b-19034dba1b65', '9a5c83a9-155b-4660-8634-70f854a9f1f3', 'BRONZE');
+INSERT INTO user_skill VALUES ('2f3d24d7-3c3e-42d7-b92b-19034dba1b65', '81357686-0d05-4e6a-947b-f09c9fc14ad2', 'SILVER');
+INSERT INTO user_skill VALUES ('9d451193-83b7-4e0a-9f3d-c26d0db484e2', '81357686-0d05-4e6a-947b-f09c9fc14ad2', 'GOLD');
+INSERT INTO user_skill VALUES ('9d451193-83b7-4e0a-9f3d-c26d0db484e2', 'dbbf1d00-07bd-4281-b2bd-74f86160a90b', 'GOLD');
+INSERT INTO user_skill VALUES ('1f4a386c-c934-4911-b129-971ddc03c71a', '81357686-0d05-4e6a-947b-f09c9fc14ad2', 'SILVER');
+INSERT INTO user_skill VALUES ('f6b16f15-3e95-4bea-a7d9-44d6a7a9f6ce', '97a3a207-828e-43be-8865-1903434a0a26', 'GOLD');
+INSERT INTO user_skill VALUES ('f6b16f15-3e95-4bea-a7d9-44d6a7a9f6ce', 'e72610b2-3c72-4036-b111-ac6c9a21ad3e', 'GOLD');
+INSERT INTO user_skill VALUES ('e13e5f9e-6787-4452-896b-78565017b6b7', '431e353d-682d-4f09-af85-9dfe72da0866', 'GOLD');
+INSERT INTO user_skill VALUES ('f0894a63-5e24-46a4-a9b6-21fb742f6473', '18edc2c2-e6b6-4f04-9181-1640c2813a7a', 'GOLD');
+INSERT INTO user_skill VALUES ('75a8ba57-686a-4251-8696-3ca8745781fd', '18edc2c2-e6b6-4f04-9181-1640c2813a7a', 'GOLD');
